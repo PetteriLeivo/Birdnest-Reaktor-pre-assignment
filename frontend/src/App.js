48056f8743247c599985dbd1e5bcadd904f06d88
PetteriLeivo <petteri.leivo@gmail.com>
2022-12-19 10:03:38 +0200

    print drones on canvas and changes to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,66 +2,79 @@ import React, { useState, useEffect, useRef } from 'react'
 import axios from "axios"
 import Canvas from './components/Canvas'
 import { v4 } from 'uuid'
+import droneicon from './icons/drone.png'
+import birdicon from './icons/bird.png'
 
-const SerialNumberWithCoordinates = ({serialWithCoordinates}) => {
+
+
+const SerialNumberWithCoordinates = ({ serialWithCoordinates }) => {
   if (serialWithCoordinates.length > 0) {
-    console.log("true")
-    const serialNumberWithCoordinatesElement = serialWithCoordinates.map(x => <div key={v4()}> {x.serialNumber} {x.coordinateX / 1000} {x.coordinateY/1000}</div>)
+    const serialNumberWithCoordinatesElement = serialWithCoordinates.map(x => <div key={v4()}> {x.serialNumber} {x.coordinateX / 1000} {x.coordinateY / 1000}</div>)
     return <div>{serialNumberWithCoordinatesElement}</div>
   }
-  console.log("false")
-
   return <div>Ladataan</div>
 
 }
 
-const RuleBreakers = ({serialWithCoordinates}) => {
+const RuleBreakers = ({ serialWithCoordinates, userInfoAndDistanceRef }) => {
 
   if (serialWithCoordinates.length > 0) {
-   const rulebreakers =  serialWithCoordinates.map(element => {
-      if(checkIfAboveNest(element.coordinateX/1000, element.coordinateY/1000)) {
 
-        
-
-
-        return <div key={v4()}>{element.serialNumber}</div>
+    serialWithCoordinates.forEach(element => {
+      if (checkIfAboveNest(element.coordinateX / 1000, element.coordinateY / 1000)) {
+        axios.get('http://localhost:3001/latestrulebreaker/', {
+          params: { serialNumber: element.serialNumber }
+        }).then((response) => {
+          let x = 250000 - element.coordinateX
+          let y = 250000 - element.coordinateY
+          let distance = Math.floor(Math.sqrt(x * x + y * y)) / 1000
+          userInfoAndDistanceRef.current.push({ id: element.serialNumber, firstName: response.data.firstName, lastName: response.data.lastName, distance: distance })
+        }).catch((error) => {
+          console.log(error)
+        })
       }
-      return <div key={v4()}></div>
+
+
+
     })
-    return (
-      <div>{rulebreakers}</div>
-    )
+      const userInfoAndDistance = userInfoAndDistanceRef.current.map(userInfoAndDistance => <div key={v4()}>{userInfoAndDistance.firstName} {userInfoAndDistance.lastName} {userInfoAndDistance.distance}</div>)
+      return (
+        <div>{userInfoAndDistance}</div>
+      )
+    
+
   }
-  return (
-    <div>Ei näytettävää</div>
-  )
+
 }
 
+
+
+
+
 const checkIfAboveNest = (x, y) => {
-  if (Math.pow((x-250), 2) + Math.pow((y-250), 2) <= Math.pow(100, 2)) {
+  if (Math.pow((x - 250), 2) + Math.pow((y - 250), 2) <= Math.pow(100, 2)) {
     return true
   }
   return false
 
 }
 
+
 const App = () => {
-  const serialWithCoordinatesRef = useRef();
   const [serialWithCoordinates, setSerialWithCoordinates] = useState([]);
+  const userInfoAndDistanceRef = useRef(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const dronesData = await axios.get("http://localhost:3001")
         const droneCapture = dronesData.data.report.capture.map(x => x.drone)
-        console.log(droneCapture)
-        console.log(droneCapture[0])
         const droneInfo = droneCapture[0].map(x => {
           const serialNumberandCoordinateObject = { serialNumber: x.serialNumber, coordinateX: x.positionX, coordinateY: x.positionY }
           return serialNumberandCoordinateObject
         })
-        serialWithCoordinatesRef.current = droneInfo
-        setSerialWithCoordinates(serialWithCoordinatesRef.current)
+
+        setSerialWithCoordinates(droneInfo)
       }
       catch (error) {
         console.log(error)
@@ -69,34 +82,42 @@ const App = () => {
     }
     const interval = setInterval(() => {
       fetchData()
-
-
     }, 2000)
     return () => clearInterval(interval)
   }, [])
 
   const draw = ctx => {
-    ctx.beginPath()
-    ctx.arc(250, 250, 100, 0, 2 * Math.PI)
-    ctx.fillStyle = "green"
+    ctx.arc(1.5 * 250, 1.5 * 250, 1.5 * 100, 0, 2 * Math.PI)
+    const birdImage = new Image();
+    birdImage.src = birdicon
+    birdImage.onload = () => {
+      ctx.drawImage(birdImage, 1.5 * 230, 1.5 * 230, 1.5 * 40, 1.5 * 40)
+    }
+
     if (serialWithCoordinates.length > 0) {
+      ctx.clearRect(0, 0, 750, 750)
+
       serialWithCoordinates.forEach(element => {
-        ctx.fillRect(Math.floor(element.coordinateX/1000), Math.floor(element.coordinateY/1000), 5, 5)
+        const image = new Image();
+        image.src = droneicon
+        image.onload = () => {
+          ctx.drawImage(image, 1.5 * Math.floor(element.coordinateX / 1000 - 12, 5), 1.5 * Math.floor(element.coordinateY / 1000 - 12, 5), 1.5 * 25, 1.5 * 25)
+        }
       })
     }
-    ctx.stroke() 
-}
+    ctx.stroke()
+    userInfoAndDistanceRef.current = []
+  }
+
 
 
 
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>Birdnest</h1>
-      <Canvas width="500px" height="500px" draw={draw}/>
-      <SerialNumberWithCoordinates serialWithCoordinates={serialWithCoordinates} />
-      <h2>Rule breakers</h2>
+      <RuleBreakers serialWithCoordinates={serialWithCoordinates} userInfoAndDistanceRef={userInfoAndDistanceRef} />
+      <Canvas width="750px" height="750px" draw={draw} />
 
-      <RuleBreakers serialWithCoordinates={serialWithCoordinates} />
     </div>
   )
 }
